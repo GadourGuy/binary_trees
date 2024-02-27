b6f30bf318d46fabd59d0927728c764fd3b0160f
Your Name <you@example.com>
2024-02-27 18:40:32 +0800

    fixing task 1 again

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,15 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
-	if (parent == NULL || new == NULL)
+	if (parent == NULL)
 		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
 		new->left = parent->left;
