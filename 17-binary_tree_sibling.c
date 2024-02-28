8ddc2c741eb1601ff1054f5f12a86445de14942e
Your Name <you@example.com>
2024-02-28 22:29:16 +0800

    fixing task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,15 +8,9 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent == NULL)
-		return (NULL);
-	binary_tree_t *tmp = node->parent;
-
-	if (tmp->left == node)
-		return (tmp->right);
-	else
-		return (tmp->left);
-	return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
