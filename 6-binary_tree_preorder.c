1949c2272dc8693be4d8ab941365bb35b46ffaa5
Your Name <you@example.com>
2024-02-28 13:48:58 +0800

    trying a fix for task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,9 +8,12 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL && !func)
+	if (tree == NULL)
 		return;
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	if (func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
