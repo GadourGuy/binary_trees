0f7e09e0f93a6545054d27026470c4e47b6fbf3f
Your Name <you@example.com>
2024-02-28 13:50:27 +0800

    trying some fixing for task 7 && 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,9 +8,12 @@
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL && !func)
+	if (tree == NULL)
 	return;
-	binary_tree_inorder(tree->left, func);
-	func(tree->n);
-	binary_tree_inorder(tree->right, func);
+	if (func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,9 +8,12 @@
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL && !func)
+	if (tree == NULL)
 	return;
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
