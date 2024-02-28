f5a6d630a701aa07b8a0d4620acd31daec5ddfbf
Your Name <you@example.com>
2024-02-28 14:17:39 +0800

    fixing task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
@@ -25,7 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			height++;
 			tree = tree->right;
 		}
-		else
+		else if (tree->right == NULL &&  tree->left == NULL)
 			break;
 	}
 	return (height);
